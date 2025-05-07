b52e405bf62c5feb44e680465cb338233c338d85
Alexander Grachev <awgrachev@gmail.com>
2025-05-06 23:57:50 -0700

    Refactor 'even_or_odd' to use ternary operator in version 2

--- 8kyu/ruby/even_or_odd.rb
+++ 8kyu/ruby/even_or_odd.rb
@@ -1,7 +1,7 @@
 # Create a function that takes an integer as an argument
 # and returns "Even" for even numbers or "Odd" for odd numbers.
 
-# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/ruby
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
 
 # Version 1: Using if/else
 def even_or_odd(number)
@@ -12,10 +12,24 @@ def even_or_odd(number)
     end  
   end
 
-puts "Testing even_or_odd:"
-puts even_or_odd(1) == "Odd"     # => true
-puts even_or_odd(2) == "Even"    # => true
-puts even_or_odd(-1) == "Odd"    # => true
-puts even_or_odd(-2) == "Even"   # => true
-puts even_or_odd(0) == "Even"    # => true
+# Version 2: Using the ternary operator and the even? method
+# The ternary operator is a shorthand for an if-else statement. 
+# It has the following syntax:
+#   condition ? true_result : false_result
+def even_or_odd_v2(number)
+    number.even? ? "Even" : "Odd"
+  end
   
+puts "Testing even_or_odd_v1:"
+puts even_or_odd(1) == "Odd"     # Expected output: "Odd"
+puts even_or_odd(2) == "Even"    # Expected output: "Even"
+puts even_or_odd(-1) == "Odd"    # Expected output: "Odd"
+puts even_or_odd(-2) == "Even"   # Expected output: "Even"
+puts even_or_odd(0) == "Even"    # Expected output: "Even"
+
+puts "Testing even_or_odd_v2:"
+puts even_or_odd_v2(1) == "Odd"     # Expected output: "Odd"
+puts even_or_odd_v2(2) == "Even"    # Expected output: "Even"
+puts even_or_odd_v2(-1) == "Odd"    # Expected output: "Odd"
+puts even_or_odd_v2(-2) == "Even"   # Expected output: "Even"
+puts even_or_odd_v2(0) == "Even"    # Expected output: "Even"
